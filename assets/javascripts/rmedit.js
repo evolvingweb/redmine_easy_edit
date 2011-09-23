8582790dd1f4423fc9eaa222824be46934d24c03
Thomas Getgood <thomas@evolvingweb.ca>
2011-09-23 15:45:34 -0400

    Added ctrl key activation. Getting a bit ugly...

--- assets/javascripts/rmedit.js
+++ assets/javascripts/rmedit.js
@@ -1,6 +1,10 @@
 jQuery(function ($) {
  	
 	var highlightOnClick = function(highlightSet) {
+  	// For Alex's irreversible habits...
+  	if (event && ! event.ctrlKey) {
+  		return;
+  	}
 		var that = this;
 		if (this.effectRunning) {
 			return;
@@ -14,7 +18,11 @@ jQuery(function ($) {
 	};
 	
 // 	Click handler for wiki pages.
-  $('body.controller-wiki .wiki').dblclick(function () {
+  $('body.controller-wiki .wiki').dblclick(function (event) {
+  	// For Alex's irreversible habits...
+  	if (! event.ctrlKey) {
+  		return;
+  	}
   	var temporaryLocation = document.location.href;
 // Kill everything after the hashmark if it exists.
   	temporaryLocation = temporaryLocation.replace(/#.*$/, '');
@@ -25,45 +33,65 @@ jQuery(function ($) {
     }
     // add edit and send in the location
     temporaryLocation += '/edit';
+    
     document.location = temporaryLocation;
-  })
+  });
 //  subtask editing
-  $('body.controller-issues #issue_tree').dblclick(function(e) {
-  	var element = $(this).find('.contextual a:eq(0)');
-  	if (element.attr('onclick')) {
-  		element.click();
-  	}
-  	else {
-  		document.location = element.attr('href');
-  	}
-//  	e.stopPropagation();
-  })
-  .click(function () {highlightOnClick.call(this);});
+  $('body.controller-issues #issue_tree')
+  	.dblclick(function(event) {
+	  		// For Alex's irreversible habits...
+	  	if (event && ! event.ctrlKey) {
+	  		return;
+	  	}
+	  	var element = $(this).find('.contextual a:eq(0)');
+	  	if (element.attr('onclick')) {
+	  		element.click();
+	  	}
+	  	else {
+	  		document.location = element.attr('href');
+	  	}
+	  })
+	  .click(function (event) {highlightOnClick.call(this);});
 //  issue comments
   
   $('body.controller-issues .wiki.editable')
-    .dblclick(function () {
+    .dblclick(function (event) {
+    	// For Alex's irreversible habits...
+    	if (event && ! event.ctrlKey) {
+    		return;
+    	}
   		$(this).find('.contextual a:eq(1)').click();
     })
-    .click(function () {highlightOnClick.call(this);});
+    .click(function (event) {highlightOnClick.call(this);});
 // update issue 
   $('body.controller-issues .issue.details .attributes')
-  	.dblclick(function () {
+  	.dblclick(function (event) {
+  		// For Alex's irreversible habits...
+    	if (event && ! event.ctrlKey) {
+    		return;
+    	}
 	  	$('#content .contextual a:eq(0)').click();
 	  })
-	  .click(function () {highlightOnClick.call(this);});
+	  .click(function (event) {highlightOnClick.call(this);});
   // Edit description
   var set = $('body.controller-issues .issue.details .wiki').prev().andSelf();
-  	set.dblclick(function (e) {
-		  	$('#content .contextual a:eq(0)').click();
-		  	$('#update .tabular legend a:eq(0)').click();
-		  })
-		  .click(function () {highlightOnClick.call(this, set);});
+  	set.dblclick(function (event) {
+  		// For Alex's irreversible habits...
+    	if (event && ! event.ctrlKey) {
+    		return;
+    	}
+	  	$('#content .contextual a:eq(0)').click();
+	  	$('#update .tabular legend a:eq(0)').click();
+	  })
+	  .click(function (event) {highlightOnClick.call(this, set);});
   // Related issues
-  $('body.controller-issues #relations').dblclick(function (e) {
+  $('body.controller-issues #relations').dblclick(function (event) {
+  	// For Alex's irreversible habits...
+  	if (event && ! event.ctrlKey) {
+  		return;
+  	}
   	$(this).find('.contextual a:eq(0)').click();
-//  	e.stopPropagation();
   })
-  .click(function () {highlightOnClick.call(this);});
+  .click(function (event) {highlightOnClick.call(this);});
 });
 
