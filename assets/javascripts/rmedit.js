902dbd42412cca573ea0cf3c433adaa8d9543b1c
Alex Dergachev <alex@catnip.ewdev.ca>
2011-10-05 01:51:33 -0400

    removed useless jquery.sendkeys plugin; incorporated adapted form of jQuery.scrollToText() plugin to scroll the textarea; fixed behavior when clicking on a.wiki-anchor links within a heading

--- assets/javascripts/rmedit.js
+++ assets/javascripts/rmedit.js
@@ -1,5 +1,49 @@
+/*
+ * Adapted from http://stackoverflow.com/questions/5458655/jquery-scroll-textarea-to-given-position
+ * Buggy, hacky but kinda works in initial testing.
+ */
+jQuery.fn.scrollToText = function(search) {
+  // getting given textarea contents
+  var text = jQuery(this).text();
+  // number of charecter we want to show
+
+  //EWHACK we're assuming the arg is a regexp
+  var charNo = text.search(search);
+  
+  // this SPAN will allow up to determine given charecter position
+  //EWHACK 
+  var anch = '<span height="1px" id="anch">.</span>';
+  // inserting it after the character into the text
+  text = text.substring(0, charNo) + anch + text.substring(charNo);
+
+  // creating a DIV that is an exact copy of textarea
+  var copyDiv = jQuery('<div id="copy"></div>')
+                  .append(text.replace(/\n/g, '<br />')) // making newlines look the same
+                  .css('width', jQuery(this).attr('clientWidth')) // width without scrollbar
+                  .css('font-size', jQuery(this).css('font-size'))
+                  .css('font-family', jQuery(this).css('font-family'))
+                  .css('padding', jQuery(this).css('padding'));
+
+  // inserting new div after textarea - this is needed beacuse .position() wont work on invisible elements
+  copyDiv.insertAfter(jQuery(this));
+  // what is the position on SPAN relative to parent DIV?
+  var pos = copyDiv.find('SPAN#anch').offset().top - copyDiv.find('SPAN#anch').closest('DIV').offset().top;
+
+  // the text we are interested in should be at the middle of textearea when scrolling is done
+
+  //EWHACK this line borks; is it necessary for us?
+  //pos = pos - Math.round(jQuery(this).attr('clientHeight') / 2);
+  if (pos > 100) {
+    pos = pos - 100;
+  }
+ 
+  // now, we know where to scroll!
+  jQuery(this).scrollTop(pos);
+  // no need for DIV anymore
+  copyDiv.remove();
+};
+
 jQuery(function ($) {
- 	
 	var highlightOnClick = function(highlightSet) {
 		var that = this;
   	// For Alex's irreversible habits...
@@ -41,8 +85,8 @@ jQuery(function ($) {
     var elem = $('body.controller-wiki textarea.wiki-edit');
     // Workaround for bug in jquery.caret
     if (elem.length) {
+      elem.scrollToText(term);
       elem.caret(term);
-     // elem.sendkeys('{downarrow}');
     }
   };
   
@@ -54,13 +98,34 @@ jQuery(function ($) {
     caretMove(caretSearch);
   }
 
+  var setCaretCookie = function (event) {
+    var clickedElem = event.target;
+    //if clicked on link, try acting on the parent heading
+    if (clickedElem instanceof HTMLAnchorElement && 
+        clickedElem.className == "wiki-anchor" && 
+        clickedElem.parentElement instanceof HTMLHeadingElement) {
+      clickedElem = clickedElem.parentElement;
+    }
+    //only act on clicked headings
+    if (!clickedElem instanceof HTMLHeadingElement) {
+      return false;
+    }
+    var wikiTarget =  clickedElem.localName + '\.\ +' + clickedElem.firstChild.textContent;
+    $.cookie('redmine-easy-edit-caret-wiki-target', wikiTarget, {'path' : '/'});
+  }
+
 // 	Click handler for wiki pages.
   $('body.controller-wiki .wiki')
     .dblclick(function (event) {
-        var wikiTarget =  event.target.localName + '\.\ +' + event.target.firstChild.textContent;
-        $.cookie('redmine-easy-edit-caret-wiki-target', wikiTarget, {'path' : '/'});
+        setCaretCookie(event);
         // console.log(event); 
         doWhatRedmineDoes('body.controller-wiki #content .contextual a:contains("Edit")');
+        return false;
+    }).click(function (event) {
+      //prevent "wiki-anchor" links within heading from being control-clickable
+      if(event.target.className == "wiki-anchor"  && (event.metaKey || event.ctrlKey)) {
+        return false;
+      }
     });
 
 //  Subtask editing
--- lib/ewwiki_edit_hook.rb
+++ lib/ewwiki_edit_hook.rb
@@ -6,8 +6,6 @@ class EWWikiEditHook < Redmine::Hook::ViewListener
 #      javascript_include_tag("jquery.caret.1.02.js", :plugin => 'redmine_easy_edit')+
       javascript_include_tag("jquery.caret.1.02.min.js", :plugin => 'redmine_easy_edit')+
       javascript_include_tag("jquery.cookie.js", :plugin => 'redmine_easy_edit')+
-      javascript_include_tag("bililiteRange.js", :plugin => 'redmine_easy_edit')+
-      javascript_include_tag("jquery.sendkeys.js", :plugin => 'redmine_easy_edit')+
       javascript_include_tag("rmedit.js", :plugin => 'redmine_easy_edit')
   end
 end
