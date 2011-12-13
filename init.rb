387e2e525548def0316a559bc546606c78ff4872
Thomas Getgood <thomas@evolvingweb.ca>
2011-12-13 18:06:50 -0500

    Now loading only the js files needed on each page.

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 require 'redmine'
 
-require_dependency 'ewwiki_edit_hook'
+require_dependency 'ew_easy_edit_hook'
 
 Redmine::Plugin.register :redmine_easy_edit do
   name "Easy Edit Plugin"
   author 'Thomas Getgood'
   description 'Enable double click edit feature as in mediawiki.'
-  version '0.0.1'
+  version '0.0.6'
 end
 
---
+++ lib/ew_easy_edit_hook.rb
@@ -0,0 +1,33 @@
+require_dependency 'application_helper'
+
+class EWEasyEditHook < Redmine::Hook::ViewListener
+  def view_layouts_base_html_head(context={})
+
+    # N.B. Try to include as little js as possible. 
+
+    controller = context[:controller]
+
+    return unless controller.class.name == "WikiController" or controller.class.name == "IssuesController"
+
+    files = %w{jquery.min.js jquery.noconflict.js} 
+
+    if controller.class.name == "IssuesController" then
+      files += %w{issue-easy-edit.js}
+    elsif controller.class.name == "WikiController" then
+    
+      request = context[:request]
+      action = request.parameters[:action]
+
+      if action == "show" then
+        files += %w{wiki-easy-edit.js jquery.cookie.js}
+      elsif action == "edit" then
+        files += %w{wiki-edit-jump.js jquery.cookie.js jquery.caret.1.02.min.js diff_match_patch.js}
+      end
+    end
+
+    files.map do |file| 
+      javascript_include_tag(file, :plugin=> 'redmine_easy_edit')
+    end
+  end
+end
+
