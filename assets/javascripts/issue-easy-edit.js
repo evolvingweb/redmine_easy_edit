2508a6be52b154d0951af36c9b7ced83c5da6ae7
Amir Kadivar <amir@evolvingweb.ca>
2015-01-27 17:09:39 -0500

    pass the event object explicitly to handlers
    `event' is not necessarily magically populated.

--- assets/javascripts/issue-easy-edit.js
+++ assets/javascripts/issue-easy-edit.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
 
   //  Literally.
-  var doWhatRedmineDoes = function (elements) {
+  var doWhatRedmineDoes = function (event, elements) {
     // For Alex's irreversible habits...
     if ($('#loggedas a').text() !== "thomas" && event && !(event.ctrlKey || event.metaKey)) {
       return;
@@ -21,31 +21,31 @@ jQuery(function ($) {
   //  Subtask editing
   $('body.controller-issues #issue_tree')
     .dblclick(function (event) {
-      doWhatRedmineDoes($(this).find('.contextual a:contains("Add")'));
+      doWhatRedmineDoes(event, $(this).find('.contextual a:contains("Add")'));
     });
 
   //  Issue comments
   $('body.controller-issues .wiki.editable')
     .dblclick(function (event) {
-      doWhatRedmineDoes($(this).find('.contextual a[title="Edit"]'));
+      doWhatRedmineDoes(event, $(this).find('.contextual a[title="Edit"]'));
     });
 
   // Update issue
   $('body.controller-issues .issue.details .attributes')
     .dblclick(function (event) {
-      doWhatRedmineDoes('#content .contextual a:contains("Update")');
+      doWhatRedmineDoes(event, '#content .contextual a:contains("Update")');
     });
 
 
   // Edit description
   var set = $('body.controller-issues .issue.details .wiki, body.controller-issues .issue.details .subject').prev().andSelf();
     set.dblclick(function (event) {
-      doWhatRedmineDoes('#content .contextual a:contains("Update"), #update .tabular legend a:contains("More")');
+      doWhatRedmineDoes(event, '#content .contextual a:contains("Update"), #update .tabular legend a:contains("More")');
     });
 
   // Related issues
   $('body.controller-issues #relations').dblclick(function (event) {
-    doWhatRedmineDoes($(this).find('.contextual a:contains("Add")'));
+    doWhatRedmineDoes(event, $(this).find('.contextual a:contains("Add")'));
   });
 
 });
