54b92072600f5bdc64ae0005aa8ee75daf68fdd2
Burak Ikiler <123000591+burak-ikiler-ew@users.noreply.github.com>
2024-05-22 11:47:51 +0300

    Update init.rb for version upgrade

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ Redmine::Plugin.register :redmine_easy_edit do
   name "Easy Edit Plugin"
   author 'Thomas Getgood'
   description 'Enable double click edit feature as in mediawiki.'
-  version '0.0.6'
+  version '0.0.7'
 end
 
